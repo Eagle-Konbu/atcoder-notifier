4203a047161b8e0b4d94ca0b57cee0e708a1778b
Atsuya Uchida <atsuya_eagle@outlook.com>
2023-01-28 03:55:53 +0900

    Added simple lambda code.

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime", from: "1.0.0-alpha"),
+        .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime.git", from: "1.0.0-alpha"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- Sources/atcoder-notifier/main.swift
+++ Sources/atcoder-notifier/main.swift
@@ -1,1 +1,13 @@
-print("Hello, world!")
+import AWSLambdaRuntime
+
+#if DEBUG
+try Lambda.withLocalServer {
+    Lambda.run { (context: Lambda.Context, event: String, callback: @escaping (Result<String, Error>) -> Void) in
+        callback(.success("Hello, world!"))
+    }
+}
+#else
+Lambda.run { (context: Lambda.Context, event: String, callback: @escaping (Result<String, Error>) -> Void) in
+    callback(.success("Hello, world!"))
+}
+#endif
