82991848a15850eebe7d40552809e4906f66ad1d
Atsuya Uchida <atsuya_eagle@outlook.com>
2023-02-09 04:06:37 +0900

    Fixed build.sh.

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ rm -rf "$target"
 mkdir -p "$target"
 
 cp ".build/release/Notifier" "$target/"
+cp -Pv /usr/lib/swift/linux/lib*so* "$target"
 cd "$target"
 ln -s "Notifier" "bootstrap"
 zip --symlinks lambda.zip *
