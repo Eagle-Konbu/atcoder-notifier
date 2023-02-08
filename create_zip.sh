bd0910255a3802e7242587914069b2a666519d30
Atsuya Uchida <atsuya_eagle@outlook.com>
2023-02-09 02:30:25 +0900

    Removed redundant cp.

--- create_zip.sh
+++ create_zip.sh
@@ -6,6 +6,5 @@ rm -rf "$target"
 mkdir -p "$target"
 
 cp ".build/release/Notifier" "$target/"
-cp -Pv /usr/lib/swift/linux/lib*so* "$target/"
 ln -s "Notifier" "bootstrap"
 zip --symlinks lambda.zip *
