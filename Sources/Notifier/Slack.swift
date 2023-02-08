bdae0ea1fc7906396318f28238f82b37d70b3c55
Atsuya Uchida <atsuya_eagle@outlook.com>
2023-02-08 18:36:04 +0900

    Added Slack.swift

---
+++ Sources/Notifier/Slack.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+public class Slack {
+    
+}
+
+// Body Format
+//{
+//    "blocks": [
+//        {
+//            "type": "header",
+//            "text": {
+//                "type": "plain_text",
+//                "text": ":deployparrot: 今週のAtCoderコンテスト  :deployparrot:"
+//            }
+//        },
+//        {
+//            "type": "divider"
+//        },
+//        {
+//            "type": "section",
+//            "text": {
+//                "type": "plain_text",
+//                "text": "AGC061\n01/02 (日) 21:00 開始",
+//                "emoji": true
+//            },
+//            "accessory": {
+//                "type": "button",
+//                "text": {
+//                    "type": "plain_text",
+//                    "text": "参加登録"
+//                },
+//                "value": "click_me_123",
+//                "action_id": "button-action",
+//                "url": "https://atcoder.jp/contests/agc061"
+//            }
+//        }
+//    ]
+//}
